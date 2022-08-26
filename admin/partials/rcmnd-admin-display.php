02bcafb9c8aa01bb3f205b0ae7bd13fb670fb9ab
denisRPD <111157569+denisRPD@users.noreply.github.com>
2022-08-26 23:36:00 +0200

    Sanitize added to activeTab variable

--- admin/partials/rcmnd-admin-display.php
+++ admin/partials/rcmnd-admin-display.php
@@ -23,7 +23,9 @@
 		</div>
 	</div>
 	<?php
-		$active_tab = isset( $_GET[ "tab"] ) ? $_GET["tab"] : "general";
+		$active_tab_string = isset( $_GET[ "tab"] ) ? $_GET["tab"] : "general";
+		$active_tab = filter_var($active_tab_string, FILTER_SANITIZE_STRING);
+	
 		$active_general= ($active_tab=='general' ? 'nav-tab-active':'');
 		$active_advanced= ($active_tab=='advanced' ? 'nav-tab-active':'');
 	?>
