0a79416b524f23dd35ff912d169f4da224be8c55
denisRPD <111157569+denisRPD@users.noreply.github.com>
2023-04-11 17:53:00 +0200

    RPD-2385-v-1-3-9

--- rcmnd.php
+++ rcmnd.php
@@ -18,7 +18,7 @@
  * Description:       Recommend Referral Integration Plugin.
  * Version:           1.3.9
  * Author:            Recommend Inc.
- * Author URI:        https://about.recommend.co
+ * Author URI:        https://www.recommend.co/for-developers
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
  * Text Domain:       rcmnd
